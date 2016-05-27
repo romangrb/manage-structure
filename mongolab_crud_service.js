50e7de86836ea4549b09ba11e9828680b7e924b3
Roman <grabarroman@gmail.com>
2016-05-27 10:21:08 +0000

    fixed crud module

--- mongolab_crud_service.js
+++ mongolab_crud_service.js
@@ -1,28 +1,31 @@
+(function(){
+  
+  'use strict';
+  
 // This is a module for cloud persistance in mongolab - https://mongolab.com
-angular.module('mongolab', ['ngResource']).
-  factory('Project', function($resource) {
-    var Project = $resource('https://api.mongolab.com/api/1/databases' +
-      '/grbdb/collections/organizations/:id',
-      { apiKey: 'umQLTHlfoM-UB68t6YdiiCzRDByzOUQg' }, 
-        {
-          update: { method: 'PUT' }
-        }
-    );
-    
-    
-    
-   /* 
-   "https://api.mongolab.com/api/1/databases/YOUR-DATABASE
-   /collections/YOUR-COLLECTION/" + id + "?apiKey=YOUR-API-KEY")*/
+// "https://api.mongolab.com/api/1/databases/YOUR-DATABASE
+// /collections/YOUR-COLLECTION/" + id + "?apiKey=YOUR-API-KEY")
 
-    Project.prototype.update = function(cb) {
-      return Project.update({id: this._id.$oid},
-        angular.extend({}, this, {_id:undefined}), cb);
-    };
+  angular.
+      module('mongolab', ['ngResource']).
+      factory('Project', function($resource) {
+         
+          var Project = $resource(
+            'https://api.mongolab.com/api/1/databases/grbdb/collections/organizations/:id',
+            { apiKey: 'umQLTHlfoM-UB68t6YdiiCzRDByzOUQg' }, 
+            { update: { method: 'PUT' } }
+          );
 
-    Project.prototype.destroy = function(cb) {
-      return Project.remove({id: this._id.$oid}, cb);
-    };
-
-    return Project;
-  });
+          Project.prototype.update = function(cb) {
+            return Project.update({id: this._id.$oid},
+              angular.extend({}, this, {_id:undefined}), cb);
+          };
+      
+          Project.prototype.destroy = function(cb) {
+            return Project.remove({id: this._id.$oid}, cb);
+          };
+      
+          return Project;
+      });
+  
+})();
