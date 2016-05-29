7b935dbe5fae4e88e4e5aa15d0fc9214d60b5db3
Roman <grabarroman@gmail.com>
2016-05-29 08:44:58 +0000

    provided descendanse search

--- data_service.js
+++ data_service.js
@@ -46,95 +46,69 @@
        
     function PrivProtMeth (){
       
-      var self = this;
-      
-      this.__tmp_collection = '';
-      
-      var _slice = Array.prototype.slice;
-      
-      this.__copyArray = function(args){
+        var self = this;
         
-        return _slice.call(args);
+        this.__tmp_collection = '';
         
-      };
-      
-      this.__searchPotentialParents = function(q){
+        var _slice = Array.prototype.slice;
         
-        var collection = self.__tmp_collection;
-        
-        if (collection==null || collection.length<1) return '';
-        //if (arr!=null&&arr.length>0)  
-        var thisCompany = collection.find(function(item) {
-          return item._id.$oid == q.id;
-        });
-        
-        var descendants = [];
-        
-        getDescendants(thisCompany);
-        
-        function getDescendants(company){
-                
-            if (company.child_ids!=null && company.child_ids.length>0) {
-                
-                var companies = company.child_ids;  
-                    
-                collection.forEach(function(item, j, arr) {
-                  
-                    companies.forEach(function(targChild, i, targArr) {
-                    
-                        if (targChild == item._id.$oid) {
-                            
-                            descendants.push(item);
-                            arr.splice(j, 1);
-                          
-                        }
-                    
-                    });
-                    
-                });
-                
-                getDescendants(descendants);
-                console.log(descendants, collection); 
-              
-            }
-            
-            
-        }
-        
-        
-        //}
-        
-        //CompanyFactory(successCb);
-        
-        function successCb(collections){
-         
-         /* var potentialParents = [],
-            arr = collections,
-            ln = arr.length;
-            
-          for (var i = 0; i < ln; i++) {
-            
-            var childrens = arr[i].child_ids;
-            
-            if (childrens!=null&&childrens.length>0){  
-              
-                arr.forEach(function(item, i, arr) {
-                    
-                    if (item===self.original._id.$oid) {
-                      arr.child_ids.splice(i, 1);
-                    }
-                
-                });
-         
-            }    
-              
-              
-              console.log(potentialParents);
-              
-            }*/
+        this.__copyArray = function(args){
           
-        }
+          return _slice.call(args);
+          
+        };
+      
+        this.__searchPotentialParents = function(q){
+          
+          var collection = self.__tmp_collection;
+          
+          if (collection==null || collection.length<1) return '';
+          //if (arr!=null&&arr.length>0)  
+          var thisCompany = collection.find(function(item) {
+            return item._id.$oid == q.id;
+          });
+          
+          var descendants = [],
+            tmp_descendants = [],
+            new_tmp_descendants = [];
+          
+          getDescendants(thisCompany);
+          
+          function getDescendants(company){
+                  
+              if (company.child_ids!=null && company.child_ids.length>0) {
+                  
+                  var companies = company.child_ids;  
+                      
+                  collection.forEach(function(item, j, arr) {
+                    
+                      companies.forEach(function(targChild, i, targArr) {
+                          if (targChild == item._id.$oid) tmp_descendants.push(item);
+                      });
+                      
+                  });
+                  
+                  if (tmp_descendants.length>0){
+                    
+                      descendants = descendants.concat(tmp_descendants);
+                      
+                      new_tmp_descendants = self.__copyArray(tmp_descendants);
+                      
+                      tmp_descendants = [];
+                      
+                      new_tmp_descendants.forEach(function(new_item) {
+                          getDescendants(new_item);  
+                      });
+                 
+                  }
+                  
+              }
+                  
+          }
+            
+      console.log(descendants,'desc');
         
+    
       };
   
     }
